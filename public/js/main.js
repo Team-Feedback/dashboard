48fb9c914f1531692005bb2dc39c4abda4e11190
markrajk <marko88rajkovic@gmail.com>
2020-11-12 15:13:27 +0100

    update comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,33 @@
 //HEADER SMALL/BIG TOGGLE
-let header = document.querySelector('.header');
-let headerMain = document.querySelector('.header-main');
+let header = document.querySelector(".header");
+let headerMain = document.querySelector(".header-main");
 
-headerMain.addEventListener('click', function () {
-    console.log('double click')
-    header.classList.toggle('small')
+headerMain.addEventListener("click", function () {
+  console.log("double click");
+  header.classList.toggle("small");
 });
 
 //ACTIVE LINK
-let headerNavLinks = document.querySelectorAll('.header-nav-list-item');
+let headerNavLinks = document.querySelectorAll(".header-nav-list-item");
 
-headerNavLinks.forEach(e => {
-    e.addEventListener('click', function() {
-        headerNavLinks.forEach(x => x.classList.remove('active'));
-        this.classList.add('active')
-    })
-})
+headerNavLinks.forEach((e) => {
+  e.addEventListener("click", function () {
+    headerNavLinks.forEach((x) => x.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
 
 //SETTINGS TABLE LINK
-let editLinks = document.querySelectorAll('.settings-table-row-column-03 .settings-table-link');
+let editLinks = document.querySelectorAll(
+  ".settings-table-row-column-03 .settings-table-link"
+);
 
-editLinks.forEach(e => {
-    e.addEventListener('click', function() {
-    console.log('clicked')
-    this.parentElement.parentElement.classList.toggle('edit');
-    })
-})
+editLinks.forEach((e) => {
+  e.addEventListener("click", function () {
+    console.log("clicked");
+    this.parentElement.parentElement.classList.toggle("edit");
+  });
+});
 
 //ACCORDION
 let accordionTriggers = document.querySelectorAll(".accordion-card-trigger");
@@ -40,40 +42,58 @@ accordionTriggers.forEach((e) => {
   });
 });
 
-        //MODAL
-        let modalTriggers = document.querySelectorAll(".modal-trigger");
-        let modalCloseBtns = document.querySelectorAll("[close-modal]");
-        console.log(modalCloseBtns);
+//MODAL
+let modalTriggers = document.querySelectorAll(".modal-trigger");
+let modalCloseBtns = document.querySelectorAll("[close-modal]");
+console.log(modalCloseBtns);
 
-        modalTriggers.forEach((e) => {
-            e.addEventListener("click", function () {
-            let modal = document.getElementById(
-                `${e.getAttribute("target-modal")}`
-            );
-            modal.classList.add("open");
-            });
-        });
+modalTriggers.forEach((e) => {
+  e.addEventListener("click", function () {
+    let modal = document.getElementById(`${e.getAttribute("target-modal")}`);
+    modal.classList.add("open");
+  });
+});
 
-        modalCloseBtns.forEach((e) => {
-            e.addEventListener("click", function () {
-            let modal = document.getElementById(
-                `${e.getAttribute("close-modal")}`
-            );
-            modal.classList.remove("open");
-            });
-        });
+modalCloseBtns.forEach((e) => {
+  e.addEventListener("click", function () {
+    let modal = document.getElementById(`${e.getAttribute("close-modal")}`);
+    modal.classList.remove("open");
+  });
+});
 
-        //SEND FEEDBACK
-        let sendFeedbackBtn = document.querySelectorAll(".send-feedback-modal");
+//SEND FEEDBACK
+let sendFeedbackBtn = document.querySelectorAll(".send-feedback-modal");
 
-        if (sendFeedbackBtn) {
-            sendFeedbackBtn.forEach(e => {
-            e.addEventListener("click", function () {
-                let modal = this.parentElement.parentElement;
-                let body = modal.querySelector('.custom-modal-body');
-                let successMessage = modal.querySelector('.success-message')
-                successMessage.style.maxHeight = `${body.clientHeight / parseFloat(getComputedStyle(document.documentElement).fontSize)}em`
-                modal.classList.add("success");
-            });
-            })
-        }
+if (sendFeedbackBtn) {
+  sendFeedbackBtn.forEach((e) => {
+    e.addEventListener("click", function () {
+      let modal = this.parentElement.parentElement;
+      let body = modal.querySelector(".custom-modal-body");
+      let successMessage = modal.querySelector(".success-message");
+      successMessage.style.maxHeight = `${
+        body.clientHeight /
+        parseFloat(getComputedStyle(document.documentElement).fontSize)
+      }em`;
+      modal.classList.add("success");
+    });
+  });
+}
+
+//INPUT PASSWORD VISIBILLITY
+let showPassToggle = document.querySelectorAll(
+  ".input-container.password .show-password"
+);
+
+if (showPassToggle.length) {
+  showPassToggle.forEach((e) => {
+    e.addEventListener("click", function () {
+      let input = this.parentElement.querySelector("input");
+      this.classList.toggle("show");
+      if (this.classList.contains("show")) {
+        input.type = "text";
+      } else {
+        input.type = "password";
+      }
+    });
+  });
+}
